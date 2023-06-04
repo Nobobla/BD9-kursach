e35b943327a829ce45502ad92716c435882d90a8
Лев Студеникин <lyustudenikin@edu.hse.ru>
2023-06-04 21:21:05 +0500

    Мелкие правки

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+п»їusing System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml.Style;
+using OfficeOpenXml;
 
 namespace BD9.Pages.Orders
 {
@@ -41,6 +43,62 @@ namespace BD9.Pages.Orders
 
             return RedirectToPage();
         }
+
+        public IActionResult OnPostExportToExcel(CancellationToken ct)
+        {
+
+            var allOrders = context.Orders
+                .Include(x => x.Service)
+                .Include(x => x.Emp).ThenInclude(x => x.ContactInform)
+                .Include(x => x.Complaints)
+                .Include(x => x.Client)
+                .Include(x => x.Complaints)
+                .AsNoTracking().ToList();
+
+            var stream = new MemoryStream();
+            using (var package = new ExcelPackage(stream))
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Orders");
+                worksheet.Cells[1, 1].Value = "РЈСЃР»СѓРіР°";
+                worksheet.Cells[1, 2].Value = "РљР»РёРµРЅС‚";
+                worksheet.Cells[1, 3].Value = "РњР°СЃС‚РµСЂ";
+                worksheet.Cells[1, 4].Value = "Р“Р°СЂР°РЅС‚РёСЏ";
+                worksheet.Cells[1, 5].Value = "РћРїРёСЃР°РЅРёРµ";
+                worksheet.Cells[1, 6].Value = "Р”Р°С‚Р° РѕР±СЂР°С‰РµРЅРёСЏ";
+                worksheet.Cells[1, 7].Value = "Р–Р°Р»РѕР±Р°";
+
+                int row = 2;
+                foreach (var ord in allOrders)
+                {
+                    worksheet.Cells[row, 1].Value = ord.Service.ServiceName;
+                    worksheet.Cells[row, 2].Value = ord.Client.Surname;
+                    worksheet.Cells[row, 3].Value = ord.Emp.ContactInform.Name;
+                    worksheet.Cells[row, 4].Value = ord.Warranty;
+                    worksheet.Cells[row, 5].Value = ord.Description;
+                    worksheet.Cells[row, 6].Value = ord.DateIssue;
+                    worksheet.Cells[row, 7].Value = ord.Complaints.Discription;
+
+                    row++;
+                }
+
+                using (var range = worksheet.Cells[1, 1, 1, 7])
+                {
+                    range.Style.Font.Bold = true;
+                    range.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    range.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                }
+
+                package.Save();
+            }
+
+            stream.Position = 0;
+            string fileName = "Orders.xlsx";
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+
+
+        }
+
+
     }
 }
 //using System.Collections.Generic;
@@ -89,24 +147,24 @@ namespace BD9.Pages.Orders
 
 //        public IActionResult OnPostExportToExcel()
 //        {
-//            // Получаем данные из базы данных
+//            // РџРѕР»СѓС‡Р°РµРј РґР°РЅРЅС‹Рµ РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С…
 //            List<Order> data = context.Orders.AsNoTracking().ToList();
 
-//            // Создаем новый пакет Excel
+//            // РЎРѕР·РґР°РµРј РЅРѕРІС‹Р№ РїР°РєРµС‚ Excel
 //            ExcelPackage package = new ExcelPackage();
 
-//            // Создаем новый лист Excel
+//            // РЎРѕР·РґР°РµРј РЅРѕРІС‹Р№ Р»РёСЃС‚ Excel
 //            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Orders");
-//            // Заполняем заголовки столбцов
-//            worksheet.Cells[1, 1].Value = "Наименование услуги";
-//            worksheet.Cells[1, 2].Value = "Клиент";
-//            worksheet.Cells[1, 3].Value = "Мастер";
-//            worksheet.Cells[1, 4].Value = "Дата принятия заказа";
-//            worksheet.Cells[1, 5].Value = "Гарантия";
-//            worksheet.Cells[1, 6].Value = "Описание состояния телефона";
-//            worksheet.Cells[1, 7].Value = "Жалоба";
+//            // Р—Р°РїРѕР»РЅСЏРµРј Р·Р°РіРѕР»РѕРІРєРё СЃС‚РѕР»Р±С†РѕРІ
+//            worksheet.Cells[1, 1].Value = "РќР°РёРјРµРЅРѕРІР°РЅРёРµ СѓСЃР»СѓРіРё";
+//            worksheet.Cells[1, 2].Value = "РљР»РёРµРЅС‚";
+//            worksheet.Cells[1, 3].Value = "РњР°СЃС‚РµСЂ";
+//            worksheet.Cells[1, 4].Value = "Р”Р°С‚Р° РїСЂРёРЅСЏС‚РёСЏ Р·Р°РєР°Р·Р°";
+//            worksheet.Cells[1, 5].Value = "Р“Р°СЂР°РЅС‚РёСЏ";
+//            worksheet.Cells[1, 6].Value = "РћРїРёСЃР°РЅРёРµ СЃРѕСЃС‚РѕСЏРЅРёСЏ С‚РµР»РµС„РѕРЅР°";
+//            worksheet.Cells[1, 7].Value = "Р–Р°Р»РѕР±Р°";
 
-//            // Заполняем данные
+//            // Р—Р°РїРѕР»РЅСЏРµРј РґР°РЅРЅС‹Рµ
 //            for (int i = 0; i < data.Count; i++)
 //            {
 //                worksheet.Cells[i + 2, 1].Value = data[i].ServiceId;
@@ -118,7 +176,7 @@ namespace BD9.Pages.Orders
 //                worksheet.Cells[i + 2, 7].Value = data[i].ComplaintId;
 //            }
 
-//            // Сохраняем файл Excel
+//            // РЎРѕС…СЂР°РЅСЏРµРј С„Р°Р№Р» Excel
 //            byte[] fileContents = Encoding.UTF8.GetBytes(package.GetAsByteArray());
 //            string fileName = "Orders.xlsx";
 
